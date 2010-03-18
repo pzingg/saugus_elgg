ea8e61976bd3bc870b9801017ff186e8c001bdae
Peter Zingg <peter@zingg.name>
2010-03-17 18:07:03 -0700

    terms of use

--- content/mainindex/content_main_terms.php
+++ content/mainindex/content_main_terms.php
@@ -2,34 +2,55 @@
 
     $sitename = sitename;
     $run_result .= <<< END
-<p><b>If you use the $sitename web site you agree to be bound by the following terms and conditions ('Terms of Use').</b></p>
+<h1>$sitename Terms and Conditions</h1>
+<p>If you use the $sitename web site you agree to be bound by the following terms and conditions ('Terms of Use').</p>
 
 <p><b>Basic Terms</B></p>
 
-<p> 1. You must be 13 years and older to use this site.<br />
-   2. You are responsible for any activity that occurs under your screen name.<br />
-   3. You are responsible for keeping your password secure.<br />
-   4. You must not abuse, harass, threaten, impersonate or intimidate other $sitename users.<br />
-   5. You may not use the $sitename service for any illegal or unauthorized purpose. International users agree to comply with all local laws regarding online conduct and acceptable content.<br />
-   6. You are solely responsible for your conduct and any data, text, information, screen names, graphics, photos, profiles, audio and video clips, links ('Content')that you submit, post, and display on the $sitename service.<br />
-   7. $sitename is intended for personal use, not a business solution. Professional or corporate uses of $sitename are at your own risk.<br />
-   8. You must not modify, adapt or hack $sitename or modify another website so as to falsely imply that it is associated with $sitename.<br />
-   9. You must not create or submit unwanted email to any $sitename members ('Spam').<br />
-  10. You must not transmit any worms or viruses or any code of a destructive nature.<br />
-  11. You must not, in the use of $sitename, violate any laws in your jurisdiction (including but not limited to copyright laws).<br /></p>
+<p> 1. You are responsible for any activity that occurs under your screen name.<br />
+   2. You are responsible for keeping your password secure.<br />
+   3. You must not abuse, harass, threaten, impersonate or intimidate other $sitename users.<br />
+   4. Never post student last names or other personal information. Teachers should avoid posting personal addresses, phone numbers, etc.<br />
+   5. Photos and/or videos containing actual students are not to be posted without parent permission. Do not associate student names with individual student photos, if at all possible.<br />
+   6. You may not use the $sitename service for any illegal or unauthorized purpose. International users agree to comply with all local laws regarding online conduct and acceptable content.<br />
+   7. You are solely responsible for your conduct and any data, text, information, screen names, graphics, photos, profiles, audio and video clips, links ('Content') that you submit, post, and display on the $sitename service.<br />
+   8. $sitename is intended for school district use, not a business solution. Professional or corporate uses of $sitename are prohibited.<br />
+   9. You must not modify, adapt or hack $sitename or modify another website so as to falsely imply that it is associated with $sitename.<br />
+  10. You must not create or submit unwanted email to any $sitename members ('Spam').<br />
+  11. You must not transmit any worms or viruses or any code of a destructive nature.<br />
+  12. You must not, in the use of $sitename, violate any laws in your jurisdiction (including but not limited to copyright laws).<br /></p>
 
 <p>Violation of any of these agreements will result in the termination of your $sitename account. While $sitename prohibits such conduct and content on its site, you understand and agree that $sitename cannot be responsible for the Content posted on its web site and, despite our best efforts, may be exposed to such materials and that you use the $sitename service at your own risk.<p>
 
 <p><b>General Conditions</b></p>
 <ul>
 <li>We reserve the right to modify or terminate the $sitename service for any reason, without notice at any time.</li>
-<li> We reserve the right to alter these Terms of Use at any time. If the alterations constitute a material change to the Terms of Use, we will notify you via an appropriate method. What is a 'material change' is at our discretion</li>
+<li>We reserve the right to alter these Terms of Use at any time. If the alterations constitute a material change to the Terms of Use, we will notify you via an appropriate method. What is a 'material change' is at our discretion</li>
 <li>We reserve the right to refuse service to anyone for any reason at any time.</li>
+<li>We reserve the right to modify or withdraw any content posted on the $sitename service.</li>
  </ul>
 
 <p><b>Copyright</b></p>
 
-<p><b>We claim no intellectual property rights over any material you provide to the $sitename service</b></p>
+<p>We claim no intellectual property rights over any material you provide to the $sitename service. Unless you specify otherwise, 
+any and all works of authorship copyrightable by you and posted by you are submitted under the terms of an 
+<a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_new">Attribution-ShareAlike Creative Commons Public License.</a> 
+Under this license, you permit anyone to copy, distribute, display and perform your Content, royalty-free, on the condition that they credit 
+your authorship each time they do so. You also permit others to distribute derivative works of your Content, but only if they do so under 
+the same Attribution-ShareAlike license that governs your original Content.</p>
+
+<p>If you prefer to offer your Content on more restrictive terms, label your submission with a full copyright notice, i.e., your name, the 
+word “copyright” or symbol “©” and the year of first publication.</p>
+
+<p>By posting your Content using the $sitename service, you are granting SUSD a non-exclusive, royalty-free, perpetual, and worldwide 
+license to use your Content in connection with the operation of the $sitename service, including, without limitation, the license rights to copy, 
+distribute, transmit, publicly display, publicly perform, reproduce, edit, translate and reformat your Content, and/or to incorporate 
+it into a collective work.</p>
+
+<p>When publicly displaying, publicly performing, reproducing or distributing copies of your Content, or Content as incorporated into a 
+collective work, SUSD will make best efforts to credit your authorship. You grant SUSD permission to use your name for such attribution 
+purposes. You, likewise, agree to represent yourself accurately. You acknowledge that misrepresentation may lead us, in our sole discretion, 
+to cancel your use of the the $sitename service and delete any of your Content.</p>
 END;
 
 ?>
